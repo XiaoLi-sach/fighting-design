370774e96d50a20de431f0ece07e95450f76066b
XiaoLi-sach <XiaoLi-sach@users.noreply.github.com>
2022-11-17 13:32:08 +0000

    deploy: 18ad21ed396fd517af386d6531e98685bb03d0a4

---
+++ assets/chunks/copy.56444d66.js
@@ -0,0 +1,1 @@
+import{m as t}from"../app.b3576136.js";const n=o=>{const e=document.createElement("input");e.value=o,document.body.appendChild(e),e.select(),document.execCommand("Copy"),document.body.removeChild(e),t({message:"\u590D\u5236\u6210\u529F",type:"primary"})};export{n as o};
